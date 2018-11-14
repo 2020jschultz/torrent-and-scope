71339c6c7fa905835613ecfc230db83f5187ecb2
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-11-14 14:02:14 -0600

    Add files via upload
    working, done with "file.js"

---
+++ file.js
@@ -0,0 +1,27 @@
+//type your code inside this function
+function TorrentFile(){
+
+//first function
+function numberOfPackets(fileSize){
+    return fileSize * 20;
+}
+
+
+
+
+//second function
+function buildTorrentPacketArray(numofPackets){
+      let list = [];
+      for(let i = 0; i < list.length; i++){
+      return list;
+      }
+    }
+
+
+
+
+
+}
+
+
+//main test methods
---
+++ tracker.js
@@ -0,0 +1,28 @@
+//Type your code in this function
+function Tracker(){
+
+  let seeds =[];
+  let percent = [];
+
+  //implement all six functions below
+function numOfSeeds(value){
+let result = 0;
+for(let i = null; i < list.length; i++){
+  if(list[i] === value){
+  result++;
+  }
+}
+}
+function addSeed("s"){
+
+}
+function removeSeed(){
+
+}
+function reportHealth(){
+
+}
+function update(){
+
+}
+}
