4ab05c362127b197d7e6e75a175c36ea42f5ac67
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-11-15 13:57:49 -0600

    Add files via upload
    starting on function update() on tracker, done with file......

--- tracker.js
+++ tracker.js
@@ -5,24 +5,39 @@ function Tracker(){
   let percent = [];
 
   //implement all six functions below
-function numOfSeeds(value){
-let result = 0;
-for(let i = null; i < list.length; i++){
-  if(list[i] === value){
-  result++;
+function numOfSeeds(list){
+    let result = 0;
+    for(let i = 0; i < list.length; i++){
+        if(list[i] !== null){
+        result++;
+        }
+    }
+    return result;
+}
+function addSeed(s){
+  list.push(s);
+  return list;
+}
+function removeSeed(list){
+  function findValue(name){
+    let index = 0;
+    for(let i = 0; i < list.length; i++){
+      if(list[i] === value){
+        return null;
+      }
+    }
   }
 }
-}
-function addSeed("s"){
-
-}
-function removeSeed(){
-
-}
 function reportHealth(){
-
+  function average(list){
+    let sum = 0;
+    for(let i = 0; i < list.length; i++){
+      sum = sum + list[i];
+    }
+      return sum/list.length;
+  }
 }
 function update(){
-
+  
 }
 }
