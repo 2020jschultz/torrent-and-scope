061ae7a44915cdb6801436c71d5297a1c4c8d019
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-11-28 14:04:30 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -3,6 +3,13 @@ function User(tracker, torrent){
 
   let myTracker = tracker;
   let myTorrent = torrent;
+  
+  let state = "seed";
+  
+  let downloadSpeed= 0;
+  
+  let uploadSpeed = 0;
+  
 
   //Type all of your code in here.
 
