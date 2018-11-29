fde5b370ac290443b2ebba817c9109a403968ac5
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-11-29 13:57:25 -0600

    Add files via upload
    two functions left in user, done with tracker and file

--- user.js
+++ user.js
@@ -13,7 +13,36 @@ function User(tracker, torrent){
 
   //Type all of your code in here.
 
+function startDownload(){
 
+}
+function updateTracker(){
+  function Tracker(){
+    function average(list){
+      let sum = 0;
+      for(let i = 0; i < list.length; i++){
+        sum = sum + list [i];
+      
+      }
+      return sum / list.length
+    }
+  }
+}
+function checkProgress(){
+  function TorrentFile(){
+    function average(list){
+      let sum = 0;
+      for(let i = 0; i < list.length; i++){
+        sum = sum + list[i];
+      }
+      return sum / list.length;
+    }
+  }
+}
+function leech(){
+  Math.random (0, 1);
+}
+function seed(){
 
-
+}
 }
